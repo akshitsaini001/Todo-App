1e09096dc00e9f730d3d9ea9e6b232ec1f6e8062
Akshit Saini <akshitsaini75000@gmail.com>
2026-05-26 08:19:43 +0530

    Design home page

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 import express from 'express';
 import methodOverride from 'method-override';
 import path from 'path';
+import Task from './models/task.js';
+import { fileURLToPath } from 'url';
+import mongoose from 'mongoose';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const app = express();
 const port = 8080;
 
 app.set('view engine' ,  'ejs');
 app.set("views", path.join(__dirname , "/views"));
-app.use(express.static("public"));
 app.use(express.static(path.join(__dirname , "public")));
 app.use(methodOverride('_method'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+main()
+.catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/todoApp');
+
+}
 
 import router from './router/routes.js';
 app.use("" , router);
